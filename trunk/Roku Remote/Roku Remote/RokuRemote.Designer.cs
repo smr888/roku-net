7fab7d6d29f07dd0f832ed7a60e53b4bdae54860
gbpnkans@gmail.com <gbpnkans@gmail.com@93d71d66-7660-0287-b4a0-ade028b3cbf1>
2011-02-15 07:05:38 +0000

    Initial Version of Roku Remote using .Net

---
+++ trunk/Roku Remote/Roku Remote/RokuRemote.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace Roku_Remote
+{
+    partial class RokuRemote
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(RokuRemote));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.searchtext = new System.Windows.Forms.Label();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(83, 13);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(275, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.RokuRemote_KeyPress);
+            this.textBox1.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.RokuRemote_PreviewKeyDown);
+            // 
+            // searchtext
+            // 
+            this.searchtext.AutoSize = true;
+            this.searchtext.Location = new System.Drawing.Point(12, 15);
+            this.searchtext.Name = "searchtext";
+            this.searchtext.Size = new System.Drawing.Size(68, 13);
+            this.searchtext.TabIndex = 1;
+            this.searchtext.Text = "Search Text:";
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // RokuRemote
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(378, 43);
+            this.Controls.Add(this.searchtext);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "RokuRemote";
+            this.Text = "Roku Remote";
+            this.MinimumSizeChanged += new System.EventHandler(this.RokuRemote_MinimumSizeChanged);
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.RokuRemote_FormClosing);
+            this.Load += new System.EventHandler(this.RokuRemote_Load);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.RokuRemote_KeyPress);
+            this.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.RokuRemote_PreviewKeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+             
+        
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label searchtext;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ trunk/Roku Remote/Roku Remote/RokuRemote.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace Roku_Remote
+{
+    public partial class RokuRemote : Form
+    {
+        Socket skt;
+        string rokuhost;
+        int rokuport;
+
+        public RokuRemote()
+        {            
+            InitializeComponent();
+            rokuhost = Properties.Settings.Default.rokuipaddress;
+            rokuport = Properties.Settings.Default.rokuport;
+        }
+
+        private void SendKeyPressString(string stringtosend)
+        {
+            stringtosend = "POST /keypress/" + stringtosend + " HTTP/1.1\r\n\r\n";
+            skt = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            skt.Connect(rokuhost, rokuport);
+            if (skt.Connected)
+            {
+                int bytes = skt.Send(Encoding.ASCII.GetBytes(stringtosend),SocketFlags.None);                
+                Console.WriteLine(bytes + " Bytes Sent");                
+            }
+            Thread.Sleep(100);
+            //byte [] bytes1 = new byte[8000];
+            //skt.Receive(bytes1);
+            skt.Close();
+        }
+
+        private void SendKeyAlphaString(string stringtosend)
+        {
+            // stringtosend = "POST /keypress/Lit_" + stringtosend + " HTTP/1.1\r\n\r\n";
+            char[] charstosend = stringtosend.ToArray();
+            foreach (var chartosend in charstosend)
+            {
+                if( chartosend == ' ' )
+                    SendKeyPressString("Lit_%20" + chartosend);
+                else 
+                    SendKeyPressString("Lit_" + chartosend);                
+            }
+            SendKeyPressString("Enter");
+            textBox1.Text = "";
+        }
+
+        private void RokuRemote_Load(object sender, EventArgs e)
+        {
+            notifyIcon1.Text = "Roku Remote Control";
+            notifyIcon1.DoubleClick += new EventHandler(notifyIcon1_DoubleClick);
+        }
+
+        void RokuRemote_MinimumSizeChanged(object sender, System.EventArgs e)
+        {
+            
+        }
+
+        void notifyIcon1_DoubleClick(object sender, EventArgs e)
+        {
+            this.BringToFront();
+        }
+
+        void RokuRemote_FormClosing(object sender, System.Windows.Forms.FormClosingEventArgs e)
+        {
+            e.Cancel = true;
+        }
+
+        void RokuRemote_PreviewKeyDown(object sender, System.Windows.Forms.PreviewKeyDownEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Up:
+                    SendKeyPressString("Up");
+                    break;
+                case Keys.Down:
+                    SendKeyPressString("Down");
+                    break;
+                case Keys.Right:
+                    SendKeyPressString("Right");
+                    break;
+                case Keys.Left:
+                    SendKeyPressString("Left");
+                    break;
+                case Keys.Home:
+                    SendKeyPressString("Home");
+                    break;
+                case Keys.Back:
+                    SendKeyPressString("Back");
+                    break;
+                case Keys.PageUp:
+                    SendKeyPressString("Fwd");
+                    break;
+                case Keys.PageDown:
+                    SendKeyPressString("Rev");
+                    break;
+
+            }
+        }
+
+        void RokuRemote_KeyPress(object sender, System.Windows.Forms.KeyPressEventArgs e)
+        {
+            //MessageBox.Show(e.KeyChar.ToString());
+            switch (e.KeyChar)
+            {
+                case ' ':
+                    if (textBox1.Text.Length == 0)
+                    {
+                        textBox1.Text = textBox1.Text.Trim();
+                        SendKeyPressString("Play");
+                    }                   
+                    break;
+                case '\r':
+                    if (textBox1.Text.Length > 0)
+                        SendKeyAlphaString(textBox1.Text);
+                    else
+                        SendKeyPressString("Select");
+                    break;
+                case '\b':
+                    SendKeyPressString("Back");
+                    break;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            string teststring = textBox1.Text.Trim();
+            if (teststring.Length == 0)
+                textBox1.Text = "";
+        }
+
+    }
+}
